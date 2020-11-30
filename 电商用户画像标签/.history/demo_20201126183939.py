6941e0b80f7d34067ff12da566834d731a606d54
acemyzoe <acemyzoe@github.com>
2020-11-30 11:48:56 +0800

    1130

--- 电商用户画像标签/.history/demo_20201126183939.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-import gc
-import warnings
-warnings.filterwarnings('ignore')
-from datetime import datetime
-# 导入数据集
-df_orginal = pd.read_csv('./taobao_persona_1%.csv')
--- 电商用户画像标签/.history/demo_20201126185133.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-import gc
-import warnings
-warnings.filterwarnings('ignore')
-from datetime import datetime
